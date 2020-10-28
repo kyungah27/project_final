3716da960c001a67cf9401d214e9d7f0d175a75a
Joun <romizeya@gmail.com>
2020-10-28 16:44:17 +0900

    userdao interface 정리
    userdao interface 정리

--- GIANTS_BOX/src/main/java/com/uver/user/dao/UserDao.java
+++ GIANTS_BOX/src/main/java/com/uver/user/dao/UserDao.java
@@ -1,46 +1,43 @@
 package com.uver.user.dao;
 
-import java.sql.SQLException;
 import java.util.List;
 
-import javax.sql.DataSource;
-
 public interface UserDao {
-
-	//setter를 통한 DI
-	//N사,D사
-	public void setDataSource(DataSource dataSource);
-
-	public int doUpdate(User user);
-
-	public List<User> doSelectList(User user);
-
-	public int count(User user) throws ClassNotFoundException, SQLException;
-
+	
+	/**
+	 * 사용자 등록
+	 * @param user
+	 */
+	public int doInsert(User user);
+	
 	/**
 	 * 삭제
 	 * @param user
 	 * @return 1(성공)/0(실패)
-	 * @throws ClassNotFoundException
-	 * @throws SQLException
 	 */
-	public int doDelete(User user) throws ClassNotFoundException, SQLException;
-
+	public int doDelete(User user);
+	
 	/**
-	 * 사용자 등록
+	 * 수정
 	 * @param user
-	 * @throws ClassNotFoundException
-	 * @throws SQLException
+	 * @return 1(성공)/0(실패)
 	 */
-	public int doInsert(User user) throws ClassNotFoundException, SQLException;
-
+	public int doUpdate(User user);
+	
 	/**
-	 * 등록된 사용자 정보 조회
+	 * 단건조회
 	 * @param id
 	 * @return User
-	 * @throws ClassNotFoundException
-	 * @throws SQLException
 	 */
-	public User doSelectOne(String id) throws ClassNotFoundException, SQLException;
+	public User doSelectOne(String id);
 
+	/**
+	 * 다건조회
+	 * @param user
+	 * @return List<User>
+	 */
+	public List<User> doSelectList(User user);
+
+	public int count(User user);
+	
 }
