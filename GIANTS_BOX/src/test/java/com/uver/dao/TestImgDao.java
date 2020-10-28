965e8be74a3be633d0615249377df1cdb6d930be
Joun <romizeya@gmail.com>
2020-10-28 16:56:49 +0900

    test packages generated
    test packages generated

---
+++ GIANTS_BOX/src/test/java/com/uver/dao/TestImgDao.java
@@ -0,0 +1,14 @@
+package com.uver.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestImgDao {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
