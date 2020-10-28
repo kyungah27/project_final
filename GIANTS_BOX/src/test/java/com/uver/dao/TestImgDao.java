7ccaa53cc012763e49d7a53098284f4293f2a17d
romizeya <romizeya@gmail.com>
2020-10-29 08:59:13 +0900

    Update TestImgDao.java

--- GIANTS_BOX/src/test/java/com/uver/dao/TestImgDao.java
+++ GIANTS_BOX/src/test/java/com/uver/dao/TestImgDao.java
@@ -2,13 +2,25 @@ package com.uver.dao;
 
 import static org.junit.Assert.*;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class) //스프랭 테스트 컨텍스트 프레임워크의 JUnit기능 확장
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+                                   "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
 public class TestImgDao {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
