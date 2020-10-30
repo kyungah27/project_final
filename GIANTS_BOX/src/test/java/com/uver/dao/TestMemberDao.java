352fb53f393a131c4d88885b733737cf3b298dff
eichel <66511905+eichel130@users.noreply.github.com>
2020-10-31 01:47:46 +0900

    MemberDao modified

--- GIANTS_BOX/src/test/java/com/uver/dao/TestMemberDao.java
+++ GIANTS_BOX/src/test/java/com/uver/dao/TestMemberDao.java
@@ -1,11 +1,15 @@
 package com.uver.dao;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
@@ -15,7 +19,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.uver.vo.memberVO;
+import com.uver.vo.MemberVO;
 
 //메소드 수행 순서:
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -29,16 +33,17 @@ public class TestMemberDao {
 	Logger LOG = Logger.getLogger(TestMemberDao.class);
 	
 	@Autowired
-	memberDao dao;
+	MemberDao dao;
 	
 	@Autowired
 	ApplicationContext context;
 	
-	memberVO member01;
-	memberVO member02;
-	memberVO member03;
+	MemberVO member01;
+	MemberVO member02;
+	MemberVO member03;
 	
 	@Test
+	@Ignore
 	public void test() {
 		//데이터 입력
 //		dao.doInsert(member01);
@@ -46,14 +51,45 @@ public class TestMemberDao {
 //		dao.doInsert(member03);
 		
 		//데이터삭제
-		dao.doDelete(member01);
-		dao.doDelete(member02);
-		dao.doDelete(member03);
+//		int flag = dao.doDelete(31);
+//		assertThat(flag, is(1));
+//		dao.doDelete(member02.getSeq());
+//		dao.doDelete(member03.getSeq());
 		
-		//단건조회
-//		memberVO member01 = dao.doSelectOne("H170_01");
-//		memberVO member02 = dao.doSelectOne("H170_02");
-//		memberVO member03 = dao.doSelectOne("H170_03");
+//		//단건조회
+//		memberVO resultVO = dao.doSelectOneById("H170_04");
+//		memberVO member02 = dao.doSelectOneById("H170_02");
+//		memberVO member03 = dao.doSelectOneById("H170_03");
+//		memberVO seqResultVO = dao.doSelectOne(35);
+//		LOG.debug(seqResultVO.toString());
+		
+		//다건조회(아이디(10),이름(20))
+		List<MemberVO> list = dao.doSelectList("20", "최");
+		for(int i = 0 ; i<list.size(); i++) {
+			LOG.debug("list "+ i + "번째   :" + list.get(i).toString());
+		}
+		
+		List<MemberVO> list2 = dao.doSelectList("10", "H170_02");
+		for(int i = 0 ; i<list2.size(); i++) {
+			LOG.debug("list "+ i + "번째   :" + list2.get(i).toString());
+		}
+		
+	}
+	
+	@Test
+	public void update() {
+		
+		MemberVO updateVO = new MemberVO("H170_04_U","최현우_04_U","1234","dotori130@naver.com","01012344321","19900130",1,"스릴러");
+		updateVO.setSeq(35);
+		
+		dao.doUpdata(updateVO);
+		
+		MemberVO afterUpdateVO =  dao.doSelectOne(35);
+		
+		LOG.debug(afterUpdateVO);
+		
+		
+		
 	}
 
 	
@@ -62,9 +98,9 @@ public class TestMemberDao {
 	
 	@Before
 	public void setUp() {
-		member01=new memberVO("H170_01","최현우_01","1234","dotori130@naver.com","01012344321","19900130",1,"스릴러");
-		member02=new memberVO("H170_02","최현우_02","1234","dotori130@naver.com","01012344221","19900130",1,"코믹");
-		member03=new memberVO("H170_03","최현우_03","1234","dotori130@naver.com","01012342321","19900130",1,"멜로");
+		member01=new MemberVO("H170_04","최현우_04","1234","dotori130@naver.com","01012344321","19900130",1,"스릴러");
+		member02=new MemberVO("H170_05","최현우_05","1234","dotori130@naver.com","01012344221","19900130",1,"코믹");
+		member03=new MemberVO("H170_06","최현우_06","1234","dotori130@naver.com","01012342321","19900130",1,"멜로");
 		LOG.debug("** setup() **");
 		LOG.debug("***********************************");
 		LOG.debug("context"+context);
