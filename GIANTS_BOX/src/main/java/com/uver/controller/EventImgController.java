09a546dabb4e2497d823772358f4735b0da49481
Joun <romizeya@gmail.com>
2020-11-03 10:52:04 +0900

    Update EventImgController.java

---
+++ GIANTS_BOX/src/main/java/com/uver/controller/EventImgController.java
@@ -0,0 +1,16 @@
+package com.uver.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EventImgController {
+	private static final Logger LOG = LoggerFactory.getLogger(EventImgController.class);
+	
+	
+	
+	
+	
+	
+	
+
+}
