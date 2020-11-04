b3b6c9c2a9d71c183a7d8168b1df3d4eca9f90f4
Joun <romizeya@gmail.com>
2020-11-04 16:36:32 +0900

    test 정리
    test 정리

--- GIANTS_BOX/src/test/java/com/uver/dao/TestEventImgDao.java
+++ GIANTS_BOX/src/test/java/com/uver/dao/TestEventImgDao.java
@@ -39,62 +39,55 @@ public class TestEventImgDao {
     
     EventImgVO eventImg01;
     EventImgVO eventImg02;
-    EventImgVO eventImg03;
 
 	@Before
 	public void setUp() throws Exception {
 		LOG.debug("---setup()---------------------------");
 		LOG.debug("[context] " + context);
 		LOG.debug("[EventImgDao] " + dao);
-    	
 		
-		
-		ImgVO img01 = new ImgVO(214, "originName01", "serverName01", "png", 10, "y", "2020-08-08", "regId01");
-		ImgVO img02 = new ImgVO(215, "originName02", "serverName02", "png", 10, "y", "2020-08-08", "regId02");
-		ImgVO img03 = new ImgVO(216, "originName02", "serverName02", "png", 10, "y", "2020-08-08", "regId02");
+		ImgVO img01 = new ImgVO(240, "originName01", "serverName01", "png", 10, "y", "2020-08-08", "regId01");
+		ImgVO img02 = new ImgVO(241, "originName02", "serverName02", "png", 10, "y", "2020-08-08", "regId02");
 		
 		//---이미지 시퀀스, 이벤트 시퀀스
-		eventImg01 = new EventImgVO(214, 2, img01);
-		eventImg02 = new EventImgVO(215, 2, img02);
-		eventImg03 = new EventImgVO(216, 3, img03);
+		eventImg01 = new EventImgVO(240, 2, img01);
+		eventImg02 = new EventImgVO(241, 2, img02);
     	
     	LOG.debug("[eventImg01] " + eventImg01);
     	LOG.debug("[eventImg02] " + eventImg02);
-    	LOG.debug("[eventImg03] " + eventImg03);
     	LOG.debug("----------------------------------");
 	}
 
-
-	@Test
-	@Ignore
+//	@Test
+//	@Ignore
 	public void test() {
 		LOG.debug("---test()---");
 	}
 	
-	
-	
 	@Test
 //	@Ignore
 	public void addAndGet() {
+		//---[전체 테스트]
 		
 		//---추가
 		int flag = dao.doInsert(eventImg01);
 		flag += dao.doInsert(eventImg02);
-		flag += dao.doInsert(eventImg03);
-		assertThat(flag, is(3));
+		assertThat(flag, is(2));
+		
+		//---단건 조회
+		doSelectOne();
 		
 		//---다건 조회
 		List<EventImgVO> list = dao.doSelectList(2);
 		assertThat(dao.count(eventImg01.getEventSeq()), is(list.size()));
+		
 	}
 	
-	@Test
-	@Ignore
+	
 	public void doSelectOne() {
 		//---단건 조회
 		dao.doSelectOne(eventImg01.getImgSeq());
 		dao.doSelectOne(eventImg02.getImgSeq());
-		dao.doSelectOne(eventImg03.getImgSeq());
 	}
 	
 	
