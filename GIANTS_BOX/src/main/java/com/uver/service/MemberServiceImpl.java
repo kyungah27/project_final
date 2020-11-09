00eb1c3ec7c7cb33e281a6733140569c2e7880d7
eichel130 <66511905+eichel130@users.noreply.github.com>
2020-11-09 19:26:42 +0900

    signup 수정

--- GIANTS_BOX/src/main/java/com/uver/service/MemberServiceImpl.java
+++ GIANTS_BOX/src/main/java/com/uver/service/MemberServiceImpl.java
@@ -12,82 +12,82 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import com.uver.dao.MemberDao;
 import com.uver.dao.MemberDaoImpl;
 import com.uver.vo.MemberVO;
 
-@Service("MemberServiceImpl")
-public class MemberServiceImpl {
-	final Logger LOG = LoggerFactory.getLogger(MemberServiceImpl.class);
-@Autowired
-MemberDaoImpl memberDaoImpl;
-
-public MemberServiceImpl() {}
-
-
-public void setMemberDaoImpl(MemberDaoImpl memberDaoImpl) {
-	this.memberDaoImpl = memberDaoImpl;
-}
-
-
-/**
- * 회원가입
- * @throws SQLException 
- * @throws ClassNotFoundException 
- */
-public int regPw(MemberVO inputuser) throws ClassNotFoundException, SQLException {
-	int flag = 0;
-
-	String pwPattern = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$";
+	@Service("MemberServiceImpl")
+	public class MemberServiceImpl {
+		final Logger LOG = LoggerFactory.getLogger(MemberServiceImpl.class);
+		@Autowired
+		MemberDaoImpl memberDaoImpl;
 	
-	Matcher matcher = Pattern.compile(pwPattern).matcher(inputuser.getPassword());
-	if(matcher.matches() == true) {
-		LOG.debug("회원등록이 완료되었습니다.");
-
-		flag = 1;
-	}else {
-		LOG.debug("영문자,숫자,특수문자를 조합해주세요");
-	}
-	
-	return flag;
-	
-
-}
-
-/**
- * 로그인
- * 
- * @param user
- */
-// View User loginUser 를줌  -> Controller ( Service.login(loginUser) )  -> Service(userDaoImpl.doSelectList(); ) 
-
-MemberVO inputUser = new MemberVO();
-
-public MemberVO login(MemberVO inputUser) {
-	MemberVO resultVO = null;
-	MemberVO searchMember = new MemberVO();
-	searchMember.setUserId("H");
-
-	List<MemberVO> list = memberDaoImpl.doSelectList("10", "H"); // H170_01
-
-	for (MemberVO vo : list) {
-		if (vo.getUserId().equals(inputUser.getUserId()) && vo.getPassword().equals(inputUser.getPassword())) {
-			LOG.debug("로그인에 성공하셨습니다.");
-			resultVO = vo;
+		public MemberServiceImpl() {
 		}
-	}
-	if (resultVO == null) {
-		LOG.debug("아이디 혹은 비밀번호를 확앤해주세요");
-	}
-
-	return resultVO;
-}
-
-	public void logout(HttpSession session) {
+	
+		public void setMemberDaoImpl(MemberDaoImpl memberDaoImpl) {
+			this.memberDaoImpl = memberDaoImpl;
+		}
 		
 		
-		session.invalidate();
+	
+		/**
+		 * 비밀번호조건
+		 * 
+		 * @throws SQLException
+		 * @throws ClassNotFoundException
+		 */
+		public int regPw(MemberVO inputuser) throws ClassNotFoundException, SQLException {
+			int flag = 0;
+	
+			String pwPattern = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$";
+	
+			Matcher matcher = Pattern.compile(pwPattern).matcher(inputuser.getPassword());
+			if (matcher.matches() == true) {
+				LOG.debug("회원등록이 완료되었습니다.");
+	
+				flag = 1;
+			} else {
+				LOG.debug("영문자,숫자,특수문자를 조합해주세요");
+			}
+	
+			return flag;
+	
+		}
+	
+		/**
+		 * 로그인
+		 * 
+		 * @param user
+		 */
+		// View User loginUser 를줌 -> Controller ( Service.login(loginUser) ) ->
+		// Service(userDaoImpl.doSelectList(); )
+	
+		MemberVO inputUser = new MemberVO();
+	
+		public MemberVO login(MemberVO inputUser) {
+			MemberVO resultVO = null;
+			MemberVO searchMember = new MemberVO();
+			searchMember.setUserId("H");
+	
+			List<MemberVO> list = memberDaoImpl.doSelectList("10", "H"); // H170_01
+	
+			for (MemberVO vo : list) {
+				if (vo.getUserId().equals(inputUser.getUserId()) && vo.getPassword().equals(inputUser.getPassword())) {
+					LOG.debug("로그인에 성공하셨습니다.");
+					resultVO = vo;
+				}
+			}
+			if (resultVO == null) {
+				LOG.debug("아이디 혹은 비밀번호를 확앤해주세요");
+			}
+	
+			return resultVO;
+		}
+	
+		public void logout(HttpSession session) {
+	
+			session.invalidate();
+		}
+	
 	}
-
-}
--- GIANTS_BOX/src/main/webapp/WEB-INF/views/signup.jsp
+++ GIANTS_BOX/src/main/webapp/WEB-INF/views/signup.jsp
@@ -9,9 +9,13 @@
                     <h2 class="text-primary">Sign up</h2>
                 </div>
                 <form>
+                    <div class="form-group"><label for="id">Id</label><input class="form-control item" type="text" id="id"></div>
                     <div class="form-group"><label for="name">Name</label><input class="form-control item" type="text" id="name"></div>
                     <div class="form-group"><label for="password">Password</label><input class="form-control item" type="password" id="password"></div>
-                    <div class="form-group"><label for="email">Email</label><input class="form-control item" type="email" id="email"></div><button class="btn btn-primary btn-block" type="submit">Sign Up</button></form>
+                    <div class="form-group"><label for="email">Email</label><input class="form-control item" type="email" id="email">
+                    <div class="form-group"><label for="phone">Phone</label><input class="form-control item" type="text" id="phone"></div>
+                    <div class="form-group"><label for="birthday">Birthday</label><input class="form-control item" type="text" id="birthday"></div>
+                    </div><button class="btn btn-primary btn-block" type="submit">Sign Up</button></form>
             </div>
         </section>
     </main>
