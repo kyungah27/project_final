075a9db8b2b5eb5aea22ad296d32401f7e37f6ba
numtotb <romizeya@gmail.com>
2020-11-11 15:22:18 +0900

    이벤트 등록 페이지 작업

--- GIANTS_BOX/src/main/java/com/uver/controller/MainController.java
+++ GIANTS_BOX/src/main/java/com/uver/controller/MainController.java
@@ -44,16 +44,27 @@ public class MainController {
 	}
 	
 	//--- 이벤트 view 페이지로 이동
-		@RequestMapping(value="eventView.do")
-		public String goEventView() {
-			LOG.debug("-------------------");
-			LOG.debug("eventView()");
-			LOG.debug("-------------------");
-			
-			//--- 추후 views 폴더에 main.jsp 페이지 설정
-			return "event_view";
-		}
+	@RequestMapping(value="eventView.do")
+	public String goEventView() {
+		LOG.debug("-------------------");
+		LOG.debug("eventView()");
+		LOG.debug("-------------------");
 		
+		//--- 추후 views 폴더에 main.jsp 페이지 설정
+		return "event_view";
+	}
+	
+	//--- 이벤트 등록 페이지로 이동
+	@RequestMapping(value="eventReg.do")
+	public String goEventReg() {
+		LOG.debug("-------------------");
+		LOG.debug("eventReg()");
+		LOG.debug("-------------------");
+		
+		//--- 추후 views 폴더에 main.jsp 페이지 설정
+		return "event_reg";
+	}
+	
 	
 	
 	
--- GIANTS_BOX/src/test/java/com/uver/controller/TestEventImgController.java
+++ GIANTS_BOX/src/test/java/com/uver/controller/TestEventImgController.java
@@ -1,14 +1,19 @@
 package com.uver.controller;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -17,8 +22,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.uver.cmn.Search;
 import com.uver.service.EventImgService;
 import com.uver.vo.EventImgVO;
+import com.uver.vo.ImgVO;
 
 //메소드 수행 순서:
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -27,7 +34,7 @@ import com.uver.vo.EventImgVO;
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml",
 		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml" })
 public class TestEventImgController {
-	public static final Logger LOG = (Logger) LoggerFactory.getLogger(TestEventImgController.class);
+	private static final Logger LOG = LoggerFactory.getLogger(TestEventImgController.class);
 	
 	@Autowired
 	WebApplicationContext context;
@@ -39,44 +46,45 @@ public class TestEventImgController {
 	MockMvc mockMvc;
 	
 	List<EventImgVO> eventImgs;
-	
+	ImgVO img01;
+    ImgVO img02;
+    Search search;
+    Search searchById;
+    
 	@Before
 	public void setUp() {
 		LOG.debug("----------------------");
 		LOG.debug("setup()");
 		
+		img01 = new ImgVO(1, "originName01", "serverName01", "png", 10, "y", "2020-08-08", "regId01");
+		img02 = new ImgVO(2, "originName02", "serverName02", "png", 10, "y", "2020-08-08", "regId02");
+		
 		eventImgs = Arrays.asList(
-				
-				
-				);
-				
+				new EventImgVO(1, 2, img01),
+				new EventImgVO(2, 2, img02)
+		);
+		
+		search = new Search(2, 1, 5);
+		search.setSearchSeqSub(eventImgService.getMaxImgSeq(2));
+		searchById = new Search("regId01", 1, 10);
+		
+		for (EventImgVO vo : eventImgs) {
+			LOG.debug("[List] " + vo);
+		}
+    	LOG.debug("----------------------------------");
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@Test
+	public void beans() {
+		LOG.debug("---------------------------");
+		LOG.debug("webApplicationContext:" + context);
+		LOG.debug("userService:" + eventImgService);
+		LOG.debug("---------------------------");
+
+		assertThat(context, is(notNullValue()));
+		assertThat(eventImgService, is(notNullValue()));
+	}
 	
 	@After
 	public void tearDown() {
