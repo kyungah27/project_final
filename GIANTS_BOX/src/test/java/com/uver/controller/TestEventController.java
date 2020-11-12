c303ad6cbec09d6a89ed2216f645d0d13356726e
심예지 <73457730+SIM-YEJI@users.noreply.github.com>
2020-11-12 15:45:19 +0900

    이벤트 컨트롤러

--- GIANTS_BOX/src/test/java/com/uver/controller/TestEventController.java
+++ GIANTS_BOX/src/test/java/com/uver/controller/TestEventController.java
@@ -34,6 +34,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.google.gson.Gson;
 import com.uver.cmn.Message;
+import com.uver.cmn.Search;
 import com.uver.dao.EventDaoImpl;
 import com.uver.service.EventService;
 import com.uver.vo.EventVO;
@@ -65,9 +66,9 @@ public class TestEventController {
 	public void setUp() throws Exception {
 		LOG.debug("=================");
 		LOG.debug("=setUp()=");
-		events = Arrays.asList(new EventVO(10, "test_01", "새모임01", "새로운모임01", 15, "영화01", "", "", "서울", "", "20/11/13","test_01","공포")
-		 					  ,new EventVO(20, "test_02", "새모임02", "새로운모임02", 15, "영화02", "", "", "경기", "", "20/11/13","test_02","드라마")
-		 					  ,new EventVO(30, "test_03", "새모임03", "새로운모임03", 15, "영화03", "", "", "인천", "", "20/11/13","test_03","코미디")
+		events = Arrays.asList(new EventVO(21, "test_01", "새모임01", "새로운모임01", 15, "영화01", "", "", "서울", "", "20/11/13","test_01","공포")
+		 					  ,new EventVO(22, "test_02", "새모임02", "새로운모임02", 15, "영화02", "", "", "경기", "", "20/11/13","test_02","드라마")
+		 					  ,new EventVO(23, "test_03", "새모임03", "새로운모임03", 15, "영화03", "", "", "인천", "", "20/11/13","test_03","코미디")
 		 );
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 		LOG.debug("=mockMvc=" + mockMvc);
@@ -76,11 +77,85 @@ public class TestEventController {
 	}
 
 	@Test
+	public void doSelectList() throws Exception {
+		Search search = new Search("","","",10,1);
+		search.setDiv("10");
+		MockHttpServletRequestBuilder createMessage = 
+		 MockMvcRequestBuilders.get("/event/doSelectList.do")
+		 						.param("div", search.getDiv())
+								.param("searchDiv", search.getSearchDiv())
+								.param("searchWord", search.getSearchWord())
+								.param("pageSize", search.getPageSize()+"")
+								.param("pageNum", search.getPageNum()+"");
+		
+		ResultActions resultActions =mockMvc.perform(createMessage)		
+				  .andExpect(status().is2xxSuccessful());
+		
+		String result = resultActions.andDo(print()).andReturn().getResponse().getContentAsString();
+		LOG.debug("===========================");
+		LOG.debug("=result=" + result);
+		LOG.debug("===========================");
+								
+	}
+	
+	@Test
+	@Ignore
+	public void doUpdate() throws Exception {
+		
+		eventDaoImpl.doDelete(events.get(0));
+		
+		int flag = eventDaoImpl.doInsert(events.get(0));
+		assertThat(flag, is(1));
+		
+		EventVO outVO = eventDaoImpl.doSelectOne(events.get(0));
+		EventVO event = outVO;
+		
+		event.setEventNm(event.getEventNm()+"수정");
+		event.setCapacity(10);
+		event.setContent(event.getContent()+"수정");
+		
+		MockHttpServletRequestBuilder createMessage = 
+				 MockMvcRequestBuilders.post("/event/doUpdate.do")
+				 .param("userId", event.getUserId())
+				 .param("eventNm",event.getEventNm())
+				 .param("content",event.getContent())
+				 .param("capacity",event.getCapacity()+"")
+				 .param("movieInfo",event.getMovieInfo())
+				 .param("startDt",event.getStartDt())
+				 .param("endDt",event.getEndDt())
+				 .param("location",event.getLocation())
+				 .param("targetDt",event.getTargetDt())
+				 .param("regId",event.getRegId())
+				 .param("genre",event.getGenre()); 
+		
+		ResultActions resultActions =mockMvc.perform(createMessage)		
+				  .andExpect(status().is2xxSuccessful());
+				
+				
+				String result = resultActions.andDo(print()).andReturn().getResponse().getContentAsString();
+				LOG.debug("===========================");
+				LOG.debug("=result=" + result);
+				LOG.debug("===========================");
+				
+				//json -> Message
+				Gson gson=new Gson();
+				
+				Message message = gson.fromJson(result, Message.class);
+				assertThat(message.getMsgId(),is(1+""));
+				LOG.debug("===========================");
+				LOG.debug("=message=" + message);
+				LOG.debug("===========================");
+		
+	}
+	
+	@Test
+	@Ignore
 	public void doInsert() throws Exception {
 		EventVO event = events.get(0);
 		
 		MockHttpServletRequestBuilder createMessage = 
 				 MockMvcRequestBuilders.post("/event/doInsert.do")
+				 .param("eventSeq", event.getEventSeq()+"")
 				 .param("eventNm", event.getEventNm())
 				 .param("userId", event.getUserId())
 				 .param("capacity", event.getCapacity()+"")
@@ -90,7 +165,8 @@ public class TestEventController {
 				 .param("endDt", event.getEndDt())
 				 .param("targetDt", event.getTargetDt())
 				 .param("regId", event.getRegId())
-				 .param("movieInfo", event.getMovieInfo());
+				 .param("movieInfo", event.getMovieInfo())
+				 .param("genre", event.getGenre());
 		
 		ResultActions resultActions = mockMvc.perform(createMessage)
 				.andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
